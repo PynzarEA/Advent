e2f66222133892837fea0c0b44db61c2bf4f0a3d
PynzarEA <pynzarea@gmail.ru>
2024-11-26 19:25:38 +0700

    update

--- Day1.py
+++ Day1.py
@@ -1,14 +1,13 @@
 
 def find_floor(stroka):
-    for line in stroka.split('\n'):
-        sum = 0
-        for symbol in list(line):
-            if symbol == ('('):
-                sum += 1
-            if symbol == (')'):
-                sum -= 1
-        print(sum)
+    sum = 0
+    for symbol in list(stroka):
+        if symbol == ('('):
+            sum += 1
+        if symbol == (')'):
+            sum -= 1
+    return sum
 def main():
-    find_floor(input())
+    print(find_floor(input()))
 if __name__ == '__main__':
     main()
