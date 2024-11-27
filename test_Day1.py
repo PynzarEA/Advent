9f693f3080e335c5bf5783dcf70c5d540535facb
PynzarEA <pynzarea@gmail.ru>
2024-11-27 09:30:05 +0700

    update

--- test_Day1.py
+++ test_Day1.py
@@ -4,4 +4,4 @@ from Day1 import find_floor
 def test_find_floor():
     assert 0 == find_floor("(())")
     assert 1 == find_floor("(()")
-    assert 1 == find_floor("(())()")
+    assert 0 == find_floor("(())()")
